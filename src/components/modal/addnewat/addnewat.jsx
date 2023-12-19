59a7880bdb2a3315f36bc6d6d15c8d4e36f713bf
avmochalov <al.mochalov93@gmail.com>
2023-12-19 15:16:36 +0300

    atcl component styles

--- src/components/modal/addnewat/addnewat.jsx
+++ src/components/modal/addnewat/addnewat.jsx
@@ -1,33 +1,33 @@
-// import "./addnewat.scss";
+import S from "./addnewat.module.scss";
 export default function Addnewat() {
   return (
-    <div className="wrapper">
-      <div className="container-bg">
-        <div className="modal__block">
-          <div className="modal__content">
-            <h3 className="modal__title">Новое объявление</h3>
-            <div className="modal__btn-close">
-              <div className="modal__btn-close-line"></div>
+    <div className={S.wrapper}>
+      <div className={S.container_bg}>
+        <div className={S.modal__block}>
+          <div className={S.modal__content}>
+            <h3 className={S.modal__title}>Новое объявление</h3>
+            <div className={S.modal__btn_close}>
+              <div className={S.modal__btn_close_line}></div>
             </div>
             <form
-              className="modal__form-newArt form-newArt"
+              className={`${S.modal__form_newArt} ${S.form_newArt}`}
               id="formNewArt"
               action="#"
             >
-              <div className="form-newArt__block">
+              <div className={S.form_newArt__block}>
                 <label htmlFor="name">Название</label>
                 <input
-                  className="form-newArt__input"
+                  className={S.form_newArt__input}
                   type="text"
                   name="name"
                   id="formName"
                   placeholder="Введите название"
                 />
               </div>
-              <div className="form-newArt__block">
+              <div className={S.form_newArt__block}>
                 <label htmlFor="text">Описание</label>
                 <textarea
-                  className="form-newArt__area"
+                  className={S.form_newArt__area}
                   name="text"
                   id="formArea"
                   cols="auto"
@@ -35,46 +35,46 @@ export default function Addnewat() {
                   placeholder="Введите описание"
                 ></textarea>
               </div>
-              <div className="form-newArt__block">
-                <p className="form-newArt__p">
+              <div className={S.form_newArt__block}>
+                <p className={S.form_newArt__p}>
                   Фотографии товара<span>не более 5 фотографий</span>
                 </p>
-                <div className="form-newArt__bar-img">
-                  <div className="form-newArt__img">
+                <div className={S.form_newArt__bar_img}>
+                  <div className={S.form_newArt__img}>
                     <img src="" alt="" />
-                    <div className="form-newArt__img-cover"></div>
+                    <div className={S.form_newArt__img_cover}></div>
                   </div>
-                  <div className="form-newArt__img">
+                  <div className={S.form_newArt__img}>
                     <img src="" alt="" />
-                    <div className="form-newArt__img-cover"></div>
+                    <div className={S.form_newArt__img_cover}></div>
                   </div>
-                  <div className="form-newArt__img">
-                    <div className="form-newArt__img-cover"></div>
-                    <img src="" alt="" />
-                  </div>
-                  <div className="form-newArt__img">
-                    <div className="form-newArt__img-cover"></div>
+                  <div className={S.form_newArt__img}>
+                    <div className={S.form_newArt__img_cover}></div>
                     <img src="" alt="" />
                   </div>
-                  <div className="form-newArt__img">
-                    <div className="form-newArt__img-cover"></div>
+                  <div className={S.form_newArt__img}>
+                    <div className={S.form_newArt__img_cover}></div>
+                    <img src="" alt="" />
+                  </div>
+                  <div className={S.form_newArt__img}>
+                    <div className={S.form_newArt__img_cover}></div>
                     <img src="" alt="" />
                   </div>
                 </div>
               </div>
-              <div className="form-newArt__block block-price">
+              <div className={`${S.form_newArt__block} ${S.block_price}`}>
                 <label htmlFor="price">Цена</label>
                 <input
-                  className="form-newArt__input-price"
+                  className={S.form_newArt__input_price}
                   type="text"
                   name="price"
                   id="formName"
                 />
-                <div className="form-newArt__input-price-cover"></div>
+                <div className={S.form_newArt__input_price_cover}></div>
               </div>
 
               <button
-                className="form-newArt__btn-pub btn-hov02"
+                className={`${S.form_newArt__btn_pub} ${S.btn_hov02}`}
                 id="btnPublish"
               >
                 Опубликовать
--- src/components/modal/atclsettings/atclsettings.jsx
+++ src/components/modal/atclsettings/atclsettings.jsx
@@ -1,24 +1,24 @@
-// import "./atclsettings.scss";
+import S from "./atclsettings.module.scss";
 
 export default function Atclsettings() {
   return (
-    <div className="wrapper">
-      <div className="container-bg">
-        <div className="modal__block">
-          <div className="modal__content">
-            <h3 className="modal__title">Редактировать объявление</h3>
-            <div className="modal__btn-close">
-              <div className="modal__btn-close-line"></div>
+    <div className={S.wrapper}>
+      <div className={S.container_bg}>
+        <div className={S.modal__block}>
+          <div className={S.modal__content}>
+            <h3 className={S.modal__title}>Редактировать объявление</h3>
+            <div className={S.modal__btn_close}>
+              <div className={S.modal__btn_close_line}></div>
             </div>
             <form
-              className="modal__form-newArt form-newArt"
+              className={`${S.modal__form_newArt} ${S.form_newArt}`}
               id="formNewArt"
               action="#"
             >
-              <div className="form-newArt__block">
+                <div className={S.form_newArt__block}>
                 <label htmlFor="name">Название</label>
                 <input
-                  className="form-newArt__input"
+                  className={S.form_newArt__input}
                   type="text"
                   name="name"
                   id="formName"
@@ -26,10 +26,10 @@ export default function Atclsettings() {
                   value="Ракетка для большого тенниса Triumph Pro STС Б/У"
                 />
               </div>
-              <div className="form-newArt__block">
+              <div className={S.form_newArt__block}>
                 <label htmlFor="text">Описание</label>
                 <textarea
-                  className="form-newArt__area"
+                  className={S.form_newArt__area}
                   name="text"
                   id="formArea"
                   cols="auto"
@@ -46,47 +46,47 @@ export default function Atclsettings() {
                   mollit anim id est laborum.
                 </textarea>
               </div>
-              <div className="form-newArt__block">
-                <p className="form-newArt__p">
+              <div className={S.form_newArt__block}>
+                <p className={S.form_newArt__p}>
                   Фотографии товара<span>не более 5 фотографий</span>
                 </p>
-                <div className="form-newArt__bar-img">
-                  <div className="form-newArt__img">
+                <div className={S.form_newArt__bar_img}>
+                  <div className={S.form_newArt__img}>
                     <img src="" alt="" />
-                    <div className="form-newArt__img-cover"></div>
+                    <div className={S.form_newArt__img_cover}></div>
                   </div>
-                  <div className="form-newArt__img">
+                  <div className={S.form_newArt__img}>
                     <img src="" alt="" />
-                    <div className="form-newArt__img-cover"></div>
+                    <div className={S.form_newArt__img_cover}></div>
                   </div>
-                  <div className="form-newArt__img">
-                    <div className="form-newArt__img-cover"></div>
-                    <img src="" alt="" />
-                  </div>
-                  <div className="form-newArt__img">
-                    <div className="form-newArt__img-cover"></div>
+                  <div className={S.form_newArt__img}>
+                  <div className={S.form_newArt__img_cover}></div>
                     <img src="" alt="" />
                   </div>
-                  <div className="form-newArt__img">
-                    <div className="form-newArt__img-cover"></div>
+                  <div className={S.form_newArt__img}>
+                  <div className={S.form_newArt__img_cover}></div>
+                    <img src="" alt="" />
+                  </div>
+                  <div className={S.form_newArt__img}>
+                  <div className={S.form_newArt__img_cover}></div>
                     <img src="" alt="" />
                   </div>
                 </div>
               </div>
-              <div className="form-newArt__block block-price">
+              <div className={`${S.form_newArt__block} ${S.block_price}`}>
                 <label htmlFor="price">Цена</label>
                 <input
-                  className="form-newArt__input-price"
+                  className={S.form_newArt__input_price}
                   type="text"
                   name="price"
                   id="formName"
                   value="2 200"
                 />
-                <div className="form-newArt__input-price-cover"></div>
+                <div className={S.form_newArt__input_price_cover}></div>
               </div>
 
               <button
-                className="form-newArt__btn-pub btn-hov02"
+                className={`${S.form_newArt__btn_pub} ${S.btn_hov02}`}
                 id="btnPublish"
               >
                 Сохранить
