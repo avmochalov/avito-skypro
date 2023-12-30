a00120a0ade980c015eac6a54333be89db20344d
avmochalov <al.mochalov93@gmail.com>
2023-12-30 17:34:05 +0500

    cards component

--- src/components/cardItem/CardItem.jsx
+++ src/components/cardItem/CardItem.jsx
@@ -1,6 +1,24 @@
-
-export default  CardItem() {
+import S from "./carditem.module.scss";
+export default function CardItem() {
   return (
-    <div>cardItem</div>
-  )
+    <div className={S.cards__item}>
+      <div className={`${S.cards__card} ${S.card}`}>
+        <div className={S.card__image}>
+          <a href="#" target="_blank">
+            <img src="#" alt="picture" />
+          </a>
+        </div>
+        <div className={S.card__content}>
+          <a href="" target="_blank">
+            <h3 className={S.card__title}>
+              Ракетка для большого тенниса Triumph Pro ST
+            </h3>
+          </a>
+          <p className={S.card__price}>2&nbsp;200&nbsp;₽</p>
+          <p className={S.card__place}>Санкт Петербург</p>
+          <p className={S.card__date}>Сегодня в&nbsp;10:45</p>
+        </div>
+      </div>
+    </div>
+  );
 }
